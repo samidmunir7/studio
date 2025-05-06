621376129d410b3238b482918bab5f2c6ed8ffae
Sami Munir <samidmunir7@gmail.com>
2025-05-06 03:29:53 -0400

    zephiron backend complete (user auth flow) controllers + routes + models + middleware

--- projects/zephiron/backend/server.js
+++ projects/zephiron/backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -11,10 +13,13 @@ const APP = express();
 APP.use(cors());
 APP.use(express.json());
 
+APP.use("/api/users", userRoutes);
+
 APP.get("/", (req, res) => {
   res.send("Welcome to Zephiron Server!");
 });
 
 APP.listen(PORT, () => {
   console.log(`Zephiron server is running on port http://localhost:${PORT}`);
+  connectDB();
 });
