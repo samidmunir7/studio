692b8b4c744999e82060febd0fcf3d1e24450c30
Sami Munir <samidmunir7@gmail.com>
2025-05-12 22:23:59 -0400

    implementing register auth-controller function

---
+++ projects/tt-customs/server/controllers/auth.controller.js
@@ -0,0 +1,36 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "tt_customs_jwt_secret";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign({ id: newUser._id }, JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(201).json({
+      token,
+      user: { id: newUser._id, name: newUser.name, email: newUser.email },
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Registration failed.", error: err.message });
+  }
+};
