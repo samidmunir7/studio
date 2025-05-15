4600b4f1aaa3c1775f9cdb62cf041a8fa0a2a9ed
Sami Munir <samidmunir7@gmail.com>
2025-05-15 00:48:27 -0400

    creating user.route + scaffolding tailwind/vite frontend

--- projects/career-dock/server/server.js
+++ projects/career-dock/server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}...`);
