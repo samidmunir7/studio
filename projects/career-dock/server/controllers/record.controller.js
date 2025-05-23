4a0c763007d54de3e515ea39a92c1a8152bf9671
Sami Munir <samidmunir7@gmail.com>
2025-05-23 08:58:27 -0400

    implementing controller + router to get all records per userId

--- projects/career-dock/server/controllers/record.controller.js
+++ projects/career-dock/server/controllers/record.controller.js
@@ -6,17 +6,34 @@ export const create = async (req, res) => {
     req.body;
 
   try {
-    // const user = User.findById({ userId });
-    // if (!user) {
-    //   console.log("Cannot create new record without valid user.");
-    // }
+    const user = User.findById({ userId });
+    if (!user) {
+      console.log("Cannot create new record without valid user.");
+      return res.status(500).json({
+        message: "Cannot create new record without valid user.",
+        error: "User not found.",
+      });
+    }
     const record = await Record.create(req.body);
     console.log("Record created successfully.");
-    res.status(201).json(record);
+    return res.status(201).json(record);
   } catch (err) {
     console.log("Error creating record.");
-    res
+    return res
       .status(400)
       .json({ message: "Error creating record.", error: err.message });
+  }
+};
+
+export const getAllRecordsByUserId = async (req, res) => {
+  const { userId } = req.body;
+  try {
+    const records = await Record.find({ userId: userId });
+    return res.status(200).json(records);
+  } catch (err) {
+    console.log("Error getting records for user.");
+    return res
+      .status(400)
+      .json({ message: "Error getting records for user.", error: err.message });
   }
 };
--- projects/career-dock/server/routes/record.route.js
+++ projects/career-dock/server/routes/record.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { create } from "../controllers/record.controller.js";
+import {
+  create,
+  getAllRecordsByUserId,
+} from "../controllers/record.controller.js";
 
 const recordRouter = express.Router();
 
 recordRouter.post("/create", create);
+recordRouter.post("/get-user-records", getAllRecordsByUserId);
 
 export default recordRouter;
